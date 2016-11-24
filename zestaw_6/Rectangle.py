a6279009875f2118ade2794a2ab5ececbea759ec
Patrycja Kochan <Patrycja.4.19@gmail.com>
2016-11-24 12:15:22 +0100

    Update Rectangle.py

--- zestaw_6/Rectangle.py
+++ zestaw_6/Rectangle.py
@@ -3,8 +3,18 @@
 
 from points import Point
 
+class Point:
+    "
+    pass
+
 class Rectangle:
-    """Klasa reprezentująca prostokąt na płaszczyźnie."""
+
+box = Rectangle()
+box.width = 100.0
+box.height = 200.0
+box.corner = Point()          # obiekt Point jest zagnieżdżony w obiekcie Rectangle
+box.corner.x = 0.0            # określamy atrybuty punktu
+box.corner.y = 0.0
 
     def __init__(self, x1=0, y1=0, x2=0, y2=0):
         self.pt1 = Point(x1, y1)
@@ -13,20 +23,30 @@ class Rectangle:
     def __str__(self):       # "[(x1, y1), (x2, y2)]"
 	self.pt1=Point(x1, y1)
 	self.pt1= Point(x2, y2)
-    def __repr__(self):        # "Rectangle(x1, y1, x2, y2)"
-	self.Rectangle=Point(x1, y1)
-	self.Rectangle=Point(x2, y2)
-    def __eq__(self, other):  # obsługa rect1 == rect2
-	self.
+    def __repr__(rect):        # "Rectangle(x1, y1, x2, y2)"
+	self.Rectangle=rect(x1, y1)
+	self.Rectangle=rect(x2, y2)
+    def __eq__(rect, other):  # obsługa rect1 == rect2
+	return (rect1.x1== rect2.x2) and (rect1.y1== rect2.y2)
 
 
-    def __ne__(self, other):        # obsługa rect1 != rect2
+    def __ne__(rect, other):        # obsługa rect1 != rect2
         return not self == other
 
-    def center(self):         # zwraca środek prostokąta
-	return Point(self.x + other.x, self.y + other.y)
+    def set_rect(rect, x, y, w, h):
+	rect.width = w
+	rect.height = h
+	rect.corner = Point()
+    set_point(rect.corner, x, y)
 
-    def area(self): pass            # pole powierzchni
+    def center(rect):         # zwraca środek prostokąta
+	x = rect.corner.x + rect.width/2.0
+	y = rect.corner.y + rect.height/2.0
+	center = Point()
+	set_point(center, x, y)
+	return center
 
+    def area(rect):            # pole powierzchni
+	return rect.width * rect.height
     def move(self, x, y): pass      # przesunięcie o (x, y)
 
