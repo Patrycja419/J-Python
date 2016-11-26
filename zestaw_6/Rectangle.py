e311b8994ac8423b09972bcc62063de0c76fe3ea
Patrycja Kochan <Patrycja.4.19@gmail.com>
2016-11-26 21:46:46 +0100

    Update Rectangle.py

--- zestaw_6/Rectangle.py
+++ zestaw_6/Rectangle.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 from points import Point
 
-class Point:
-   
-    pass
+class Point:pass
 
 class Rectangle:
 
@@ -15,37 +13,41 @@ box.corner = Point()          # obiekt Point jest zagnieżdżony w obiekcie Rect
 box.corner.x = 0.0            # określamy atrybuty punktu
 box.corner.y = 0.0
 
-    def __init__(self, x1=0, y1=0, x2=0, y2=0):
+	def __init__(self, x1=0, y1=0, x2=0, y2=0):
         self.pt1 = Point(x1, y1)
         self.pt2 = Point(x2, y2)
 
-    def __str__(self):       # "[(x1, y1), (x2, y2)]"
-	self.pt1=Point(x1, y1)
-	self.pt1= Point(x2, y2)
-    def __repr__(rect):        # "Rectangle(x1, y1, x2, y2)"
+	def __str__(self):       # "[(x1, y1), (x2, y2)]"
+        return "(%s, %s)" % (self.x1, self.y1)
+        return "(%s, %s)" % (self.x2, self.y2)
+
+	def __repr__(self):        # "Rectangle(x1, y1, x2, y2)"
 	self.Rectangle=rect(x1, y1)
 	self.Rectangle=rect(x2, y2)
-    def __eq__(rect, other):  # obsługa rect1 == rect2
+	def __eq__(rect, other):  # obsługa rect1 == rect2
 	return (rect1.x1== rect2.x2) and (rect1.y1== rect2.y2)
 
 
-    def __ne__(rect, other):        # obsługa rect1 != rect2
-        return not self == other
+	def __ne__(self, other):        # obsługa rect1 != rect2
+	return not self == other
 
-    def set_rect(rect, x, y, w, h):
+
+	def set_rect(rect, x, y, w, h):
 	rect.width = w
 	rect.height = h
 	rect.corner = Point()
 	set_point(rect.corner, x, y)
 
-    def center(rect):         # zwraca środek prostokąta
+	def center(rect):         # zwraca środek prostokąta
 	x = rect.corner.x + rect.width/2.0
 	y = rect.corner.y + rect.height/2.0
 	center = Point()
 	set_point(center, x, y)
 	return center
 
-    def area(rect):            # pole powierzchni
+	def area(rect):            # pole powierzchni
 	return rect.width * rect.height
-    def move(self, x, y): pass      # przesunięcie o (x, y)
+	def move(rect, x, y): 
+	return rect.width+x
+	return rect.height+y# przesunięcie o (x, y)
 
