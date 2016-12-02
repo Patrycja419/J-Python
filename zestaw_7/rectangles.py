e49faac8ff0b514a8cd7efc3188c05baec573510
Patrycja Kochan <Patrycja.4.19@gmail.com>
2016-12-02 17:34:14 +0100

    Update rectangles.py

--- zestaw_7/rectangles.py
+++ zestaw_7/rectangles.py
@@ -8,23 +8,33 @@ class Rectangle:
         self.pt1 = Point(x1, y1)
         self.pt2 = Point(x2, y2)
 
-    def __str__(self): pass         # "[(x1, y1), (x2, y2)]"
-
-    def __repr__(self): pass        # "Rectangle(x1, y1, x2, y2)"
-
-    def __eq__(self, other): pass   # obsługa rect1 == rect2
-
+    def __str__(self):         # "[(x1, y1), (x2, y2)]"
+        return "(%s, %s)" % (self.x1, self.y1)
+        return "(%s, %s)" % (self.x2, self.y2)
+    def __repr__(self):        # "Rectangle(x1, y1, x2, y2)"
+        self.Rectangle=rect(x1, y1)
+        self.Rectangle=rect(x2, y2)
+    def __eq__(self, other):   # obsługa rect1 == rect2
+        return (rect1.x1== rect2.x2) and (rect1.y1== rect2.y2)
     def __ne__(self, other):        # obsługa rect1 != rect2
         return not self == other
 
-    def center(self): pass          # zwraca środek prostokąta
-
-    def area(self): pass            # pole powierzchni
-
-    def move(self, x, y): pass      # przesunięcie o (x, y)
+    def center(rect):           # zwraca środek prostokąta
+        x = rect.corner.x + rect.width/2.0
+        y = rect.corner.y + rect.height/2.0
+        center = Point()
+        set_point(center, x, y)
+        return center
+    def area(srect):            # pole powierzchni
+        return rect.width * rect.height
+        def move(rect, x, y): 
+    
+    def move(rect, x, y):       # przesunięcie o (x, y)
+        return rect.width+x
+        return rect.height+y
 
     def intersection(self, other): pass # część wspólna prostokątów
-
+        return (self.x1)*(self.x2)=(other.y2)*(other.y2)
     def cover(self, other): pass    # prostąkąt nakrywający oba
-
+        
     def make4(self): pass           # zwraca listę czterech mniejszych
