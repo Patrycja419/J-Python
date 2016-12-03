4bdff4dc86d143d9a4d5ac97db7649ade60ac7e0
Patrycja Kochan <Patrycja.4.19@gmail.com>
2016-12-03 11:58:13 +0100

    Update tiragles.py

--- zestaw_7/tiragles.py
+++ zestaw_7/tiragles.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from points import Point
 
 class Triangle:
@@ -9,19 +12,37 @@ class Triangle:
         self.pt2 = Point(x2, y2)
         self.pt3 = Point(x3, y3)
 
-    def __str__(self): pass         # "[(x1, y1), (x2, y2), (x3, y3)]"
+    def __str__(self):          # "[(x1, y1), (x2, y2), (x3, y3)]"
+        self.pt1 = Point(x1, y1)
+        self.pt2 = Point(x2, y2)
+        self.pt3 = Point(x3, y3)
+    def __repr__(self):        # "Triangle(x1, y1, x2, y2, x3, y3)"
 
-    def __repr__(self): pass        # "Triangle(x1, y1, x2, y2, x3, y3)"
+        return "Triangle(%s, %s)" % (self.x1, self.y1)
+        return "Triangle(%s, %s)" % (self.x2, self.y2)
+        return "Triangle(%s, %s)" % (self.x3, self.y3)
+    
+    def __eq__(self, other):    # obsługa tr1 == tr2
 
-    def __eq__(self, other): pass   # obsługa tr1 == tr2
-
+         if self.x == other.x and self.y == other.y:
+            return True
+         else:
+            return False
+        
     def __ne__(self, other):        # obsługa tr1 != tr2
         return not self == other
 
-    def center(self): pass          # zwraca środek trójkąta
-
-    def area(self): pass            # pole powierzchni
-
-    def move(self, x, y): pass      # przesunięcie o (x, y)
-
+    def center(self):           # zwraca środek trójkąta
+    
+        x = self.center.x + self.width/2.0
+	    y = self.center.y + self.height/2.0
+	    center = Point()
+    	set_point(center, x, y)
+	    return center
+    
+    def area(self):             # pole powierzchni
+        return self.width * self.height/2.0
+    def move(self, x, y):       # przesunięcie o (x, y)
+        return self.width+x
+        return self.height+y
     def make4(self): pass           # zwraca listę czterech mniejszych
